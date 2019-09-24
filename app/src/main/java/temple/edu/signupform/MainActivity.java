7f90895f39e24299babf08172c1f17637691519a
Mike Lauw <tug01761@temple.edu>
2019-09-24 16:08:57 -0400

    Toast for any empty's field and letting the user know when they registered successfully.

--- app/src/main/java/temple/edu/signupform/MainActivity.java
+++ app/src/main/java/temple/edu/signupform/MainActivity.java
@@ -3,13 +3,16 @@ package temple.edu.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button save;
     EditText username, email, password, password_confirm;
+    String sUsername, sEmail, sPassword, sPassword_Confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +24,25 @@ public class MainActivity extends AppCompatActivity {
         email = (EditText) findViewById(R.id.getEmail);
         password = (EditText) findViewById(R.id.getPassword);
         password_confirm = (EditText) findViewById(R.id.getPasswordConfirm);
-
         save = (Button) findViewById(R.id.saveBtn);
 
-        
+        save.setOnClickListener(this);
 
+    }
 
+    @Override
+    public void onClick(View v)
+    {
+        //converting all the texts to string.
+        sUsername = username.getText().toString().trim();
+        sEmail = email.getText().toString().trim();
+        sPassword = password.getText().toString().trim();
+        sPassword_Confirm = password_confirm.getText().toString().trim();
+        if (sUsername.isEmpty() || sEmail.isEmpty() || sPassword.isEmpty() || sPassword_Confirm.isEmpty())
+        {
+            Toast.makeText(this, "Please enter all the information!", Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(this, "Welcome to the SignUpForm App", Toast.LENGTH_LONG).show();
     }
 }
