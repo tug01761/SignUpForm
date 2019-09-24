c07847b64dc0aed02863ce73c1f8cbbfd44fcd82
Mike Lauw <tug01761@temple.edu>
2019-09-23 21:54:56 -0400

    Assigning all the views that was declared

--- app/src/main/java/temple/edu/signupform/MainActivity.java
+++ app/src/main/java/temple/edu/signupform/MainActivity.java
@@ -3,12 +3,29 @@ package temple.edu.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button save;
+    EditText username, email, password, password_confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        username = (EditText) findViewById(R.id.getUsername);
+        email = (EditText) findViewById(R.id.getEmail);
+        password = (EditText) findViewById(R.id.getPassword);
+        password_confirm = (EditText) findViewById(R.id.getPasswordConfirm);
+
+        save = (Button) findViewById(R.id.saveBtn);
+
+        
+
+
     }
 }
